c9d1f82b39319091fe8f456bce2e4b9861098aaf
Johnny Ramirez <johnnyramirez@johnnys-mbp.fios-router.home>
2018-09-10 12:32:48 -0400

    Revert back to normal

--- Pace Conversion Calculator/Saved Map Folder/SavedMapViewController.swift
+++ Pace Conversion Calculator/Saved Map Folder/SavedMapViewController.swift
@@ -44,7 +44,6 @@ class SavedMapViewController: UIViewController, CLLocationManagerDelegate, MKMap
         performSegue(withIdentifier: "Saved Map to Saved Workouts", sender: nil)
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
